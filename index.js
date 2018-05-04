f383244668577173fa1188bb6ef4d998db135483
Shantanu <shantanu0805@gmail.com>
2018-05-04 15:03:46 +0530

    heroku dynamic port fix

--- index.js
+++ index.js
@@ -88,8 +88,8 @@ io.on('connection', function (socket) {
     });
 });
 
-http.listen(3000,function(){
-    console.log("Listening on 3000");
+http.listen(process.env.PORT || 3000,function(){
+    console.log("Listening on port : " + process.env.PORT);
 });
 
 app.get('/index', function (req, res) {
